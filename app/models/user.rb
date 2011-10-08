8f89bde26fe0f4d71f2ebb8ae665366f64b043ee
Chris Matthieu <chris@matthieu.us>
2011-10-08 09:01:19 -0700

    added uniqueness check on username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :on => :create
+  validates_uniqueness_of :username, :on => :create  
   has_many :comments
   has_many :follows
 end
